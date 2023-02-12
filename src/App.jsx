592cae59e5fa5e77154b00417b4f7d506140654c
codewithmarish <codewithmarish@gmail.com>
2023-02-12 12:01:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import GameBoard from "./components/GameBoard";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex justify-center">
+      <GameBoard />
+    </div>
+  );
+}
+
+export default App;
